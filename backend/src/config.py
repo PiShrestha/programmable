8773629613413e6723858adb9132640add88e2fa
PiShrestha <pratikstha980@gmail.com>
2024-10-19 20:11:01 -0400

    Add routes.py for processing URL/HTTP requests and config.py for Firebase configuration.

---
+++ backend/src/config.py
@@ -0,0 +1,4 @@
+class Config:
+    """Configuration settings for the Flask application."""
+    DEBUG = True  # Enable debug mode
+    FIREBASE_PROJECT_ID = 'programmable-cee8c'  # Firebase project ID
